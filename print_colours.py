6365d6055d6d2f03ffa8870086d7712187c7fe15
Jeff Elliott <jeff@fedge.ca>
2023-12-09 22:32:41 -0500

    Moved colour print functions to a separate module

---
+++ print_colours.py
@@ -0,0 +1,20 @@
+# Here is my attempt to create my first module:
+#   A simple set of functions to print in colour in the terminal
+#   SUGGESTION: import this module is "pr" or "prnt"
+
+
+def Red(skk): print("\033[91m {}\033[00m" .format(skk))
+
+def Green(skk): print("\033[92m {}\033[00m" .format(skk))
+
+def Yellow(skk): print("\033[93m {}\033[00m" .format(skk))
+
+def LightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+
+def Purple(skk): print("\033[95m {}\033[00m" .format(skk))
+
+def Cyan(skk): print("\033[96m {}\033[00m" .format(skk))
+
+def LightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+
+def Black(skk): print("\033[98m {}\033[00m" .format(skk))
